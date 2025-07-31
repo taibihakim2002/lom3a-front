a211527c38f4ead79be4f3d38d13ff12eb6bc1d4
Taibi Abdelhakim <taibihakim2002@gmail.com>
2025-07-31 10:20:49 +0100

    hid console.log

--- hooks/useApiRequest.js
+++ hooks/useApiRequest.js
@@ -11,11 +11,11 @@ const useApiRequest = () => {
     setError("");
     try {
       const response = await apiCall();
-      console.log(response);
+      // console.log(response);
       setData(response.data);
       return { success: true, data: response.data };
     } catch (err) {
-      console.log(err);
+      // console.log(err);
       let errorMsg = "حدث خطأ غير متوقع";
       if (err?.response?.data?.code) {
         errorMsg = errorMessages[err?.response?.data?.code] || errorMsg;

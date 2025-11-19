c7cb30a1c06e0e6c1866721ff66f4c3b81a92d87
taibi hakim <taibihakim2002@gmail.com>
2025-11-19 10:10:45 +0100

    All site web

--- app/page.jsx
+++ app/page.jsx
@@ -20,7 +20,7 @@ export default function HomePage() {
         <HeroSection />
         <AboutSection />
         <WhyChooseUsSection /> 
-        {/* <ServicesSection /> */}
+        <ServicesSection />
         <GallerySection />
         <HowItWorksSection />
         <TestimonialsSection />
